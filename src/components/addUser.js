b5f307f09db53880776c0746c4dd936c96a5e22b
Bhavishya Negi <psspl.trainee32@outlook.com>
2020-04-08 20:47:20 +0530

    refactor 2

--- src/components/addUser.js
+++ src/components/addUser.js
@@ -1,8 +1,6 @@
 import React, { Component } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
-import LoginNavbar from "./loginNavbar";
-import Header from "./navbar";
 
 export default class Register extends Component {
   constructor() {
--- src/components/users.js
+++ src/components/users.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import axios from "axios";
-import LoginNavbar from "./loginNavbar";
 import { Link, withRouter } from "react-router-dom";
 
 class Users extends Component {
